2baedbce9a41711a0bfec2e6db1cb1d56a24c3c5
Alberto L. Figueroa <0234621@up.edu.mx>
2023-11-29 01:04:19 -0600

    Fix login functionality and handle user redirection based on access type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,47 +19,85 @@ const medicoRoute = process.env.NEXT_PUBLIC_MEDICO_URL || 'https://google.com';
 
 function Home() {
 
-  const router = useRouter()
+  const router = useRouter();
 
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Function to handle redirection based on user type
+  const navigateTo = (tipoAcceso: any) => {
+    switch (tipoAcceso) {
+      case 'Paciente':
+        router.push(pacienteRoute);
+        break;
+      case 'Ejecutivo':
+        router.push(ejecutivoRoute);
+        break;
+      case 'Administrador':
+        router.push(`${administradorRoute}/sign-in?username=${username}`);
+        break;
+      case 'Proveedor':
+        router.push(`${proveedorRoute}/sign-in?username=${username}`);
+        break;
+      case 'Medico':
+        router.push(medicoRoute);
+        break;
+      default:
+        toast.error('Usuario no está dado de alta. Por favor, contacte a soporte.');
+    }
+  };
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    console.log('Login attempt with:', { username, password });
+    try {
+      const loginResponse = await fetch(`${apiRoute}/api/users/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const loginResult = await loginResponse.json();
+
+      if (loginResult.error) {
+        toast.error('Error en el login. Por favor, intente nuevamente.');
+      } else if (loginResult.result) {
+        navigateTo(loginResult.TipoAcceso);
+      }
+    } catch (error) {
+      console.error('Error during login', error);
+      toast.error('Error en el login. Por favor, intente nuevamente.');
+    }
+  };
 
   const handleSubmit = async (e: any) => {
+    console.log('Checking access type for:', username);
     e.preventDefault();
-
     try {
       const response = await fetch(`${apiRoute}/api/users/getTipoAcceso/${username}`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      const tipoAcceso = await response.json();
+      const data = await response.json();
 
-      if (!tipoAcceso) {
-        toast.error('Usuario no está dado de alta. Por favor, contacte a soporte.');
-      } else {
-        switch (tipoAcceso) {
-          case 'Paciente':
-            window.location.href = pacienteRoute;
-            break;
-          case 'Ejecutivo':
-            window.location.href = ejecutivoRoute;
-            break;
-          case 'Administrador':
-            window.location.href = `${administradorRoute}/sign-in?username=${username}`;
-            break;
-          case 'Proveedor':
-            window.location.href = `${proveedorRoute}/sign-in?username=${username}`;
-            break;
-          case 'Medico':
-            window.location.href = medicoRoute;
-            break;
-          default:
-            toast.error('Usuario no está dado de alta. Por favor, contacte a soporte.');
-        }
+      if (data.tipoAcceso === 'Administrador' || data.tipoAcceso === 'Proveedor') {
+        navigateTo(data.tipoAcceso);
+      }
+      if (data.error) {
+        toast.error('El usuario no existe. Por favor, intente nuevamente.');
+      }
+
+      else {
+        setShowPassword(true);
       }
     } catch (error) {
-      console.error('Hubo un error al realizar la solicitud', error);
+      console.error('Error al verificar el tipo de acceso', error);
+      toast.error('Error al verificar el tipo de acceso. Por favor, intente nuevamente.');
     }
-  }
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-50 text-black">
@@ -74,7 +112,7 @@ function Home() {
 
           <h1 className="text-3xl font-bold mt-5">Bienvenido a Kokua</h1>
 
-          <form onSubmit={handleSubmit} className="w-full mt-8">
+          <form className="w-full mt-8" onSubmit={showPassword ? handleLogin : handleSubmit}>
             <label htmlFor="username" className="block text-gray-700 text-xs font-bold mb-2">Ingresa tu username para continuar</label>
             <input
               type="text"
@@ -84,12 +122,37 @@ function Home() {
               placeholder="Username"
               className="mb-4 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
             />
-            <button
-              type="submit"
-              className="w-full bg-black text-white py-2 px-4 rounded-md hover:bg-opacity-90 transition ease-in-out duration-150"
-            >
-              Continuar
-            </button>
+            {showPassword && (
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                className="mb-4 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+              />
+            )}
+
+
+
+            {!showPassword && (
+              <button
+                type="submit"
+                className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
+              >
+                Continuar
+              </button>
+            )}
+            {showPassword && (
+              <button
+                type="submit"
+                className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-4"
+              >
+                Ingresar
+              </button>
+            )}
+
+
           </form>
           <p className="text-xs mt-4">¿No tienes cuenta? <a href="https://admin.kokuahealth.tech/Registro" className="text-blue-500"><AssignmentIndIcon /> Regístrate</a></p>
           <p className="text-xs mt-4">¿Tienes problemas? <a href="mailto:joseemiliokuri@gmail.com" className="text-blue-500"><MailIcon /> Contáctanos</a></p>
@@ -101,5 +164,6 @@ function Home() {
     </div>
   );
 }
+
 
 export default Home;
